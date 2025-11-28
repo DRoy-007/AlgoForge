fd02a5de5899e47352e99775bf6c9f8781ab41ed
DRoy-007 <roydebarpan07@gmail.com>
2025-11-28 14:32:04 +0000

    credit or debit card number validator using luh's algorithm

---
+++ card-validator.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <termios.h>
+using namespace std;
+
+char _getch(){
+    termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    char ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+bool luhnValidator(const string cardNumberInStr){
+    int sum = 0;
+    bool doublingDigit = false;
+
+    for (int i = cardNumberInStr.size() - 1; i >= 0; --i){
+        int digit = cardNumberInStr[i] - '0';
+
+        if (doublingDigit){
+            digit *= 2;
+            if (digit > 9) digit -= 9;
+        }
+
+        sum += digit;
+        doublingDigit = !doublingDigit;
+    }
+
+    return (sum % 10 == 0);
+}
+
+int main(){
+    const int LEN = 16;
+    string cardNumberInStr;
+    
+    cout << "Enter the card number: ";
+
+    while (cardNumberInStr.length() < LEN){
+        char ch = _getch();
+
+        if (isdigit(ch)){
+            cardNumberInStr += ch;
+            cout << ch;
+        }
+    }
+    cout << endl << endl;
+
+    if (luhnValidator(cardNumberInStr)) cout << "Valid" << endl;
+    else cout << "Invalid" << endl;
+    
+    return 0;
+}
